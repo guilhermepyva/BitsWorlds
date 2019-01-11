7b1c5df12e7230c43391efc48b0c1707a970be1a
MrPiva <mrpiva@gamemods.com.br>
2019-01-11 05:10:29 -0200

    Some adjusts

--- src/main/java/bab/bitsworlds/cmd/ConfigCmd.java
+++ src/main/java/bab/bitsworlds/cmd/ConfigCmd.java
@@ -5,17 +5,25 @@ import bab.bitsworlds.extensions.BWCommandSender;
 import bab.bitsworlds.extensions.BWPlayer;
 import bab.bitsworlds.gui.BWGUI;
 import bab.bitsworlds.gui.ImplGUI;
+import bab.bitsworlds.multilanguage.Lang;
 import bab.bitsworlds.multilanguage.LangCore;
+import bab.bitsworlds.multilanguage.LangMessage;
 import bab.bitsworlds.multilanguage.PrefixMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.DyeColor;
 import org.bukkit.Material;
+import org.bukkit.block.Banner;
+import org.bukkit.block.banner.Pattern;
+import org.bukkit.block.banner.PatternType;
 import org.bukkit.command.Command;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BannerMeta;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class ConfigCmd implements BWCommand, ImplGUI {
     public void run(BWCommandSender commandSender, Command command, String s, String[] strings) {
@@ -43,7 +51,7 @@ public class ConfigCmd implements BWCommand, ImplGUI {
                 this
         );
 
-        mainGui.setItem(0, new ItemStack(Material.BANNER));
+        updateCountryBannerItem(mainGui);
 
         guis.put(0, mainGui);
 
@@ -51,7 +59,93 @@ public class ConfigCmd implements BWCommand, ImplGUI {
     }
 
     @Override
-    public void clickEvent(InventoryClickEvent event) {
-        System.out.println("a");
+    public void clickEvent(InventoryClickEvent event, BWPlayer player, BWGUI gui) {
+        switch (event.getSlot()) {
+            case 0:
+                //Define the new Lang
+                LangCore.lang = LangCore.lang.ordinal() + 2 > Lang.values().length ? Lang.values()[0] : Lang.values()[LangCore.lang.ordinal() + 1];
+
+                LangCore.getClassMessage(this.getClass(), "language-updated").setKey("%%lang", ChatColor.BOLD + LangCore.lang.title).replaceKeys();
+
+                LangCore.getClassMessage(this.getClass(), "language-updated").setKey("%%lang", ChatColor.BOLD + LangCore.lang.title);
+                System.out.println(LangCore.getClassMessage(this.getClass(), "language-updated").getTranslatedMessage().message);
+
+                player.sendMessage(PrefixMessage.info.getPrefix(),
+                        LangCore.getClassMessage(this.getClass(), "language-updated").setKey("%%lang", ChatColor.BOLD + LangCore.lang.title));
+
+                updateCountryBannerItem(gui);
+
+                break;
+        }
+    }
+
+    public ItemStack getCountryBanner(Lang lang) {
+        ItemStack banner = new ItemStack(Material.BANNER);
+        BannerMeta bannerMeta = (BannerMeta) banner.getItemMeta();
+
+        switch (lang) {
+            case EN:
+                bannerMeta.setPatterns(Arrays.asList(
+                        new Pattern(DyeColor.RED, PatternType.BASE),
+                        new Pattern(DyeColor.WHITE, PatternType.STRIPE_SMALL),
+                        new Pattern(DyeColor.BLUE, PatternType.SQUARE_TOP_LEFT)
+                ));
+                break;
+            case PT:
+                bannerMeta.setPatterns(Arrays.asList(
+                        new Pattern(DyeColor.GREEN, PatternType.BASE),
+                        new Pattern(DyeColor.YELLOW, PatternType.RHOMBUS_MIDDLE),
+                        new Pattern(DyeColor.YELLOW, PatternType.RHOMBUS_MIDDLE),
+                        new Pattern(DyeColor.YELLOW, PatternType.RHOMBUS_MIDDLE),
+                        new Pattern(DyeColor.BLUE, PatternType.CIRCLE_MIDDLE),
+                        new Pattern(DyeColor.BLUE, PatternType.CIRCLE_MIDDLE)
+                ));
+                break;
+            case FR:
+                bannerMeta.setPatterns(Arrays.asList(
+                        new Pattern(DyeColor.WHITE, PatternType.BASE),
+                        new Pattern(DyeColor.BLUE, PatternType.STRIPE_BOTTOM),
+                        new Pattern(DyeColor.RED, PatternType.STRIPE_TOP)
+                ));
+                break;
+            case SP:
+                bannerMeta.setPatterns(Arrays.asList(
+                        new Pattern(DyeColor.YELLOW, PatternType.BASE),
+                        new Pattern(DyeColor.RED, PatternType.STRIPE_LEFT),
+                        new Pattern(DyeColor.RED, PatternType.STRIPE_RIGHT),
+                        new Pattern(DyeColor.RED, PatternType.FLOWER),
+                        new Pattern(DyeColor.RED, PatternType.STRIPE_LEFT)
+                ));
+        }
+
+        bannerMeta.addItemFlags(ItemFlag.HIDE_POTION_EFFECTS);
+
+        banner.setItemMeta(bannerMeta);
+        return banner;
+    }
+
+    private void updateCountryBannerItem(BWGUI gui) {
+        ItemStack countryBanner = getCountryBanner(LangCore.lang);
+        ItemMeta countryBannerMeta = countryBanner.getItemMeta();
+
+        countryBannerMeta.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + LangCore.lang.title);
+        List<String> countryBannerLore = new ArrayList<>();
+
+        for (Lang lang: Lang.values()) {
+            StringBuilder sb = new StringBuilder();
+
+            if (lang == LangCore.lang)
+                sb.append(ChatColor.AQUA);
+            else
+                sb.append(ChatColor.BLUE);
+
+            sb.append(lang.title);
+            countryBannerLore.add(sb.toString());
+        }
+
+        countryBannerMeta.setLore(countryBannerLore);
+        countryBanner.setItemMeta(countryBannerMeta);
+
+        gui.setItem(0, countryBanner);
     }
 }
