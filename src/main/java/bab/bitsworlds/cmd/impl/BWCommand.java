e9f12cc00cb50a8097feda87e05c43e2e7830546
MrPiva <mrpiva@gamemods.com.br>
2019-01-13 19:18:25 -0200

    Added tabComplete and getPermission() to BWCommand

--- src/main/java/bab/bitsworlds/cmd/impl/BWCommand.java
+++ src/main/java/bab/bitsworlds/cmd/impl/BWCommand.java
@@ -3,11 +3,15 @@ package bab.bitsworlds.cmd.impl;
 import bab.bitsworlds.extensions.BWCommandSender;
 import org.bukkit.command.Command;
 
+import java.util.List;
+
 /**
  * Simple interface to facilitate the work of commands with the plugin
  */
 public interface BWCommand {
-    String permission = null;
+    String getPermission();
 
     void run(BWCommandSender sender, Command cmd, String alias, String[] args);
+
+    List<String> tabComplete(BWCommandSender sender, Command cmd, String alias, String[] args);
 }
