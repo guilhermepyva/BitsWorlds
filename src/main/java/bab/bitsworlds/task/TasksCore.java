8f0b27b270c8ac40251fd465ec9a7ad0997452b0
MrPiva <mrpiva@gamemods.com.br>
2019-01-24 06:46:39 -0200

    Adjust in TasksCore

--- src/main/java/bab/bitsworlds/task/TasksCore.java
+++ src/main/java/bab/bitsworlds/task/TasksCore.java
@@ -4,9 +4,5 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class TasksCore {
-    public static Queue<BWTask> globalQueue;
-
-    public static void init() {
-        globalQueue = new LinkedList<>();
-    }
+    public static Queue<BWTask> globalQueue = new LinkedList<>();
 }
