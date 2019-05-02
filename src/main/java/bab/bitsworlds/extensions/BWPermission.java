d35808513819ae0ec3e269e54730e45ef564e5e4
MrPiva <mrpiva@gamemods.com.br>
2019-05-02 17:28:43 -0300

    Update mini-permission System

--- src/main/java/bab/bitsworlds/extensions/BWPermission.java
+++ src/main/java/bab/bitsworlds/extensions/BWPermission.java
@@ -1,22 +1,20 @@
 package bab.bitsworlds.extensions;
 
 public enum BWPermission {
-    MAINCMD("bitsworlds.maincmd"),
-    MAINCMD_CONFIG("bitsworlds.maincmd.config"),
-    MAINCMD_HELP("bitsworlds.maincmd.help"),
+    MAINCMD("maincmd"),
+    MAINCMD_CONFIG("maincmd.config"),
+    MAINCMD_HELP("maincmd.help"),
 
 
-    LOGS_SEE_ALL("bitsworlds.logs.see.all"),
-    LOGS_SEE_GLOBAL("bitsworlds.logs.see.global"),
-    LOGS_SEE_WORLD("bitsworlds.logs.see.world"),
+    LOGS_SEE("logs.see"),
 
-    LOGS_DESCRIPTION_ADD("bitsworlds.logs.description.add"),
-    LOGS_DESCRIPTION_MODIFY("bitsworlds.logs.description.modify")
+    LOGS_DESCRIPTION_ADD("logs.description.add"),
+    LOGS_DESCRIPTION_MODIFY("logs.description.modify")
 
     ;
 
     BWPermission(String string) {
-        this.string = string;
+        this.string = "bitsworlds." +  string;
     }
 
     public String string;
