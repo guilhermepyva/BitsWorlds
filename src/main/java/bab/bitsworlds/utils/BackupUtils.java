0017e99ab26d3d05b52357a4df1297b80c22aa8a
MrPiva <gui.andreghetto2@hotmail.com>
2019-10-27 19:58:43 -0300

    Fix translation and remove unsued import

--- src/main/java/bab/bitsworlds/utils/BackupUtils.java
+++ src/main/java/bab/bitsworlds/utils/BackupUtils.java
@@ -7,7 +7,6 @@ import org.bukkit.Bukkit;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
